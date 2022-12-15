5f3fcfb9c4c18a706e1765e42eeae3cc1fdc9c03
OsoIvanC <40841707+OsoIvanC@users.noreply.github.com>
2022-12-15 06:13:54 -0600

    UI,Musica y Menu principal
    Se hizo la UI del menu principal, la logica de navegacion entre pantallas y se agrego musica

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -2,24 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using DG.Tweening;
+using UnityEngine.UI;
 
 
 [System.Serializable]
 public struct EnemyStats
 {
     public float maxHealth;
-
     
     public float health;
-
     
     public float attackRange;
 
-    
     public float attackDamage;
-
-
 
 }
 public class EnemyController : MonoBehaviour
@@ -40,6 +35,27 @@ public class EnemyController : MonoBehaviour
     protected AudioClip attackClip;
 
 
+    [Header("UI")]
+    [SerializeField]
+    protected Slider healthBar;
+    [SerializeField]
+    protected Image healthImage;
+
+    public void HealthBar()
+    {
+        healthBar.value = stats.health;
+
+        float perc = (stats.health / stats.maxHealth) * 100;
+
+        if (perc > 75)
+            healthImage.color = Color.green;
+        else if (perc <= 75 && perc > 50)
+            healthImage.color = Color.yellow;
+        else if (perc <= 50 && perc > 25)
+            healthImage.color = new Color(255, 165, 0);
+        else
+            healthImage.color = Color.red;
+    }
 
 
     public virtual void ModifyStats(float value)
